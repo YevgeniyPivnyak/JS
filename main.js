3e6bcaa963753ff3f5ea128b7b7c8ab4725ed77a
YevgeniyPivnyak <dneprojek@gmail.com>
2019-07-17 23:02:07 +0300

    home work done

--- main.js
+++ main.js
@@ -1,30 +1,20 @@
-var myBtn = document.getElementById('myBtn');
-var myText = document.getElementById('text');
+var arrNames = [];
+console.log(arrNames);
 
+function inputFunction(){
+    let name = document.getElementById('inputForm').value;
+    console.log(name);
+    arrNames.push(name);
 
-myBtn.onclick = function () {
+    var newDiv = document.createElement('div');
+    newDiv.innerHTML = name;
+    listOfNames.appendChild(newDiv);
+}
 
-    var colorCode = '';
-    var symbolsForCode = '0123456789ffddccaabb0123456789ffddccaaebb0123456789ffddccaaeebb';
-    var max_position = symbolsForCode.length - 1;
-    for (i = 0; i < 6; ++i) {
-        position = Math.floor(Math.random() * max_position);
-        colorCode = colorCode + symbolsForCode.substring(position, position + 1);
-    }
-
-
-    var colorCode2 = '';
-    var symbolsForCode2 = 'ffddccaaaaeebb012345678901234567890123456789ffddccaaeebb000ffddccaaeebb';
-    var max_position = symbolsForCode2.length - 1;
-    for (i = 0; i < 6; ++i) {
-        position = Math.floor(Math.random() * max_position);
-        colorCode2 = colorCode2 + symbolsForCode2.substring(position, position + 1);
-    }
-
-    myText.innerHTML = '#' + colorCode;
-    myBtn.style.color = '#' + colorCode;
-    myBtn.style.background = '#' + colorCode2;
-    myText.style.color = '#' + colorCode;
-
+function clearFunction(){
+    var delNames = document.getElementById('listOfNames');
+   while (delNames.firstChild){
+    delNames.removeChild(delNames.firstChild);
+   }
 }
 
